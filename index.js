c3ffd4440257c8b003b3eee28d72928453b010e8
Arjumand Rukhsar <162131925+ArjumandRukhsar786@users.noreply.github.com>
2024-03-29 22:49:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 100000; //Dollar
+let myPin = 1234;
+const pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin number",
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance", "fast cash"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        const amountAnswer = await inquirer.prompt({
+            name: "amount",
+            type: "number",
+            message: "Enter your amount",
+        });
+        if (amountAnswer.amount > myBalance) {
+            console.log("You don't have enough money in your account to cover your transaction.");
+        }
+        else {
+            myBalance -= amountAnswer.amount;
+            console.log(`Transaction successfull your remaining balance is ${myBalance}`);
+        }
+        ;
+    }
+    else if (operationAns.operation === "fast cash") {
+        const enterAmount = await inquirer.prompt({
+            name: "amount",
+            type: "list",
+            message: "select your amount",
+            choices: ["1000", "5000", "10000", "20000", "50000"],
+        });
+        if (enterAmount.amount <= myBalance) {
+            let balance2 = myBalance - enterAmount.amount;
+            console.log(`The remaining balance is ${balance2}`);
+        }
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is :" + myBalance);
+    }
+    ;
+}
+else {
+    console.log("enter correct your pin number");
+}
